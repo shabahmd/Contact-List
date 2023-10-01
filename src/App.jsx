af6db842859a928fc9200ec13af9e498f733f589
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-01 19:58:12 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import Card from './Card'; // Make sure you have a Card component or import it from a library
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
-
-export default App
+function App() {
+  // You can define card data here or fetch it from an API
+  const cardData = [
+    {
+      id: 1,
+      title: 'Card 1',
+      description: 'This is the first card.',
+    },
+    {
+      id: 2,
+      title: 'Card 2',
+      description: 'This is the second card.',
+    },
+    {
+      id: 3,
+      title: 'Card 3',
+      description: 'This is the third card.',
+    },
+  ];
