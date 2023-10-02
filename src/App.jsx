407209eacb8ed1d15890b4362d7e56ae0d84aa21
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-03 00:10:39 +0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
-
-
 import ContactCard from './ContactCard'
+import "./App.css"
+
 const App = () => {
   return (
     <div>
-      <ContactCard name="Jerry Man" />
-      <ContactCard name="Zara" />
-      <ContactCard name="Sentinel" />
+      <ContactCard
+        avatarUrl="https://via.placeholder.com/150"
+        name="jenn Man"
+        email="Jenny the hoe@email.com"
+        age={65}
+      />
+
+      <ContactCard
+        avatarUrl="https://via.placeholder.com/150"
+        name="china Man"
+        email="Forest the simp@email.com"
+        age={235}
+      />
+
+      <ContactCard
+        avatarUrl="https://via.placeholder.com/150"
+        name="paki Man"
+        email="shahab the simp@email.com"
+        age={225}
+      />
     </div>
   )
 }
