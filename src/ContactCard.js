ccc2e1f77066a26978d4f215592b56d49a956188
Shahab <54299749+shabahmd@users.noreply.github.com>
2023-10-02 23:36:39 +0500

    Update ContactCard.js

--- src/ContactCard.js
+++ src/ContactCard.js
@@ -9,3 +9,13 @@ const ContactCard = () => {
         <div className='contact-card'>
             <img src="https://via.placeholder.com/150" alt="dany"></img>
             <div className='user-details'>
+                <p>Name: Jenny Man</p>
+                <p>Email: shahabahmed1923@gmail.com</p>
+                <button onClick={() => setShowAge(!showAge)}>Toggle Age </button>
+                {showAge && <p>25</p>}
+            </div>
+        </div>
+    )
+}
+
+export default ContactCard;
